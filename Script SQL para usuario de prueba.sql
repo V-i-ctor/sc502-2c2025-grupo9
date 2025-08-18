32c7d475a905f8a3302be4f4a012bf5e223ec6fd
V-i-ctor <114272340+V-i-ctor@users.noreply.github.com>
2025-08-17 20:51:28 -0600

    Script SQL - Usuario Prueba
    Correr este script para crear un usuario de prueba tipo joven

---
+++ Script SQL para usuario de prueba.sql
@@ -0,0 +1,5 @@
+USE salud_mental;
+
+-- Insertar usuario de prueba
+INSERT INTO usuarios (nombre, apellido, correo, password, tipo_usuario)
+VALUES ('Usuario', 'Prueba', 'usuario.prueba@example.com', '123456', 'joven');
